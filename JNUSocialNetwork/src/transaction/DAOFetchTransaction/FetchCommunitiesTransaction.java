1262885a9a3623525943c2576687e917cb601d85
JavaEEBoy <WongZeonbong@gmail.com>
2014-08-07 20:41:49 +0800

    fetch communities interface bug fixed

--- JNUSocialNetwork/src/transaction/DAOFetchTransaction/FetchCommunitiesTransaction.java
+++ JNUSocialNetwork/src/transaction/DAOFetchTransaction/FetchCommunitiesTransaction.java
@@ -17,7 +17,7 @@ public class FetchCommunitiesTransaction extends DAOTransaction{
 			throws Exception {
 		// TODO Auto-generated method stub
 		DAO dao = new DAO(em);
-		List<Community> communities = dao.read((String)params[0], (int)params[2], (int)params[3], Community.class, params[1]);
+		List<Community> communities = dao.read((String)params[0], (int)params[3], (int)params[4], Community.class, params[1], params[2]);
 		List<Map<String, Object>> results = new ArrayList<Map<String, Object>>();
 		for(Community community : communities) {
 			results.add(community.toRepresentation());
