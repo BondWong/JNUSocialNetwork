7433d4091a0ea101984311a16b96a0810774e2bd
winson <306941426@qq.com>
2014-08-28 18:43:30 +0800

    commit

---
+++ JNUSocialNetwork/WebContent/pages/js/reminding-bell.js
@@ -0,0 +1,372 @@
+/**
+ * 
+ */
+function on_bell_click(e) {
+	clearInterval(window.bellIntervalID);
+	window.bellIntervalID = null;
+	var tinyTip;
+	var tep = $(this);
+	var t = $(this).position().top;
+	var l = $(this).offset().left;
+	t += 30;
+	l -= 250;
+	var divTip = '.mentionBody';
+	tinyTip = $(divTip);
+	tinyTip.hide();
+	tinyTip.css({
+		top : t,
+		left : l
+	}).fadeIn(300);
+	e.stopPropagation();
+	show_remind_content();
+}
+
+function show_remind_content() {
+	var hasItem = false;
+	hasItem = add_messages_to_bell();
+	hasItem = add_events_to_bell();
+	if (!hasItem) {
+		$(".mentionBody-content").empty();
+		$(".mentionBody-content").append(
+				'<div id="mentionBody-no-content-item">没有消息</div>');
+	}
+
+}
+
+function add_messages_to_bell() {
+	var online_messages = sessionStorage.getItem("online_messages");
+	if (online_messages == null)
+		online_messages = {};
+	else
+		online_messages = $.parseJSON(online_messages);
+	var offline_messages = sessionStorage.getItem("offline_messages");
+	if (offline_messages == null)
+		offline_messages = {};
+	else
+		offline_messages = $.parseJSON(offline_messages);
+
+	$.each(offline_messages, function(index, val) {
+		for (var i = 0; i < val.length; i++) {
+			messages_remind(val[i]);
+		}
+	});
+
+	$.each(online_messages, function(index, val) {
+		for (var i = 0; i < val.length; i++) {
+			messages_remind(val[i]);
+		}
+	});
+
+	var online_messages_length = 0;
+	var offline_messages_length = 0;
+	for (key in online_messages) {
+		if (online_messages.hasOwnProperty(key))
+			online_messages_length++;
+	}
+	for (key in offline_messages) {
+		if (offline_messages.hasOwnProperty(key))
+			offline_messages_length++;
+	}
+	if (online_messages_length != 0 || offline_messages_length != 0)
+		return true;
+	return false;
+}
+
+function add_events_to_bell() {
+	var incomingEvents = sessionStorage.getItem("incomingEvents");
+	if (incomingEvents == null)
+		incomingEvents = {};
+	else
+		incomingEvents = $.parseJSON(incomingEvents);
+
+	$.each(incomingEvents, function(index, event) {
+		events_remind(event);
+	});
+
+	var incomingEvents_length = 0;
+	for (key in incomingEvents)
+		if (incomingEvents.hasOwnProperty(key))
+			incomingEvents_length++;
+	if (incomingEvents_length != 0)
+		return true;
+	return false;
+
+}
+
+function messages_remind(message) {
+	if ($("div.mentionBody-content #" + message.fromID).length == 0) {
+		$("div.mentionBody-content").append(
+				'<div class="NotiItem" id="' + message.fromID
+						+ '"><div class="col-lg-3"><div><img src="'
+						+ message.attributes.avatarLink + '"/></div></div>'
+						+ '<div class="col-lg-9"><div>' + message.from
+						+ '<span class="badge">1</span></div></div></div>');
+		var tempToID = message.toID;
+		var tempFromID = message.fromID;
+		var tempFrom = message.from;
+		$("div.mentionBody-content #" + message.fromID).click(
+				function(e) {
+					e.stopPropagation();
+					show_messages("message_" + tempFromID, tempToID,
+							tempFromID, tempFrom);
+					$(this).remove();
+				});
+	} else {
+		var temp = $("div.mentionBody-content #" + message.fromID + " span")
+				.text();
+		temp = parseInt(temp);
+		$("div.mentionBody-content #" + message.fromID + " span")
+				.text(temp + 1);
+	}
+
+}
+
+function events_remind(event) {
+	var head = '';
+	var description = '';
+	switch (event.name) {
+	case "CREATECOMMENT":
+		head = "added comment to your post";
+		description = "Add Comment";
+		break;
+	case "REPLYCOMMENT":
+		head = "replied your comment";
+		description = "Reply Comment";
+		break;
+	case "LIKEPOST":
+		head = "liked your post";
+		description = "Like Post";
+		break;
+	case "LIKECOMMENT":
+		head = "liked your comment";
+		description = "Like Comment";
+		break;
+	case "FOLLOW":
+		head = "followed you";
+		description = "Follow";
+		break;
+	}
+	$("div.mentionBody-content").append(
+			'<div class="NotiItem" id="' + event.data.eventID
+					+ '"><div class="col-lg-3"><div><img src="'
+					+ event.data.avatar
+					+ '" /></div></div><div class="col-lg-9"><h1>' + head
+					+ '</h1><div class="remindConent">' + event.data.name + ' '
+					+ description + '</div></div><div>');
+	var type = event.name;
+	var eventID = event.data.eventID;
+	var isOnline = event.action == null ? false : true;
+	$("div#" + event.data.eventID)
+			.click(
+					function(e) {
+						e.stopPropagation();
+						var incomingEvents = sessionStorage
+								.getItem("incomingEvents");
+						incomingEvents = $.parseJSON(incomingEvents);
+						delete incomingEvents["" + eventID];
+						sessionStorage.setItem("incomingEvents", JSON
+								.stringify(incomingEvents));
+
+						switch (type) {
+						case "CREATECOMMENT":
+							var dataString = FetchPostByID(event.data.postID);
+							notifyAddComment(event.data.commentID,
+									dataString.owner.ID,
+									dataString.owner.attributes.name,
+									dataString.publishDate,
+									dataString.attributes.content,
+									dataString.ID, dataString.likerIDs.length,
+									dataString.owner.attributes.avatarLink,
+									dataString.imageLinks);
+							break;
+						case "FOLLOW":
+							notifyFollow(event.data.ID);
+							break;
+						case "LIKECOMMENT":
+							var dataString = FetchPostByID(event.data.postID);
+							notifyLikeComment(event.data.commentID,
+									dataString.owner.ID,
+									dataString.owner.attributes.name,
+									dataString.publishDate,
+									dataString.attributes.content,
+									dataString.ID, dataString.likerIDs.length,
+									dataString.owner.attributes.avatarLink,
+									dataString.imageLinks);
+							break;
+						case "LIKEPOST":
+							var dataString = FetchPostByID(event.data.postID);
+							notifyLikePost(dataString.owner.ID,
+									dataString.owner.attributes.name,
+									dataString.publishDate,
+									dataString.attributes.content,
+									dataString.ID, dataString.likerIDs.length,
+									dataString.owner.attributes.avatarLink,
+									dataString.imageLinks);
+							break;
+						case "REPLYCOMMENT":
+							var dataString = FetchPostByID(event.data.postID);
+							notifyReplyComment(event.data.commentID,
+									event.data.toCommentID,
+									dataString.owner.ID,
+									dataString.owner.attributes.name,
+									dataString.publicDate,
+									dataString.attributes.content,
+									dataString.ID, dataString.likerIDs.length,
+									dataString.owner.attributes.avatarLink,
+									dataString.imageLinks);
+							break;
+						}
+
+						$(".arrowBack")
+								.replaceWith(
+										'<div class="arrowBack"><a href="javascript:void(0);"><span class="glyphicon glyphicon-chevron-left" id="arrowBack" style="font-size:12px;">&nbsp;</span></a></div>');
+						$('.arrowBack').click(
+								function() {
+									$(".arrowBack").replaceWith(
+											'<div class="arrowBack"></div>');
+									$(".mentionBody-content").empty();
+									show_remind_content();
+								});
+						if (!isOnline) {
+							$.ajax({
+								type : "PUT",
+								url : '../../app/event/deleteUnhandledEvent/'
+										+ USERID + '/' + eventID,
+								beforeSend : function(request) {
+									request.setRequestHeader("ID", USERID);
+								},
+								success : function(data) {
+								}
+							});
+						}
+					});
+
+}
+
+function notifyLikePost(ownerID, ownerNickName, publishDate, content, postID,
+		likeNum, postOwnerAvatar, postImage) {
+	notifyItem([], ownerID, ownerNickName, publishDate, content, postID,
+			likeNum, postOwnerAvatar, postImage);
+}
+function notifyLikeComment(commentID, ownerID, ownerNickName, publishDate,
+		content, postID, likeNum, postOwnerAvatar, postImage) {
+	var response = FetchCommentByID(commentID);
+	notifyItem([ response ], ownerID, ownerNickName, publishDate, content,
+			postID, likeNum, postOwnerAvatar, postImage);
+}
+function notifyReplyComment(commentID, toCommentID, ownerID, ownerNickName,
+		publishDate, content, postID, likeNum, postOwnerAvatar, postImage) {
+	var response = [];
+	var comment1 = FetchCommentByID(commentID);
+	var comment2 = FetchCommentByID(toCommentID);
+	response.push(comment1);
+	response.push(comment2);
+	notifyItem(response, ownerID, ownerNickName, publishDate, content, postID,
+			likeNum, postOwnerAvatar, postImage);
+}
+function notifyAddComment(commentID, ownerID, ownerNickName, publishDate,
+		content, postID, likeNum, postOwnerAvatar, postImage) {
+	var response = FetchCommentByID(commentID);
+	//response.push(commenterID);
+	//response.push(commentOwnerID);
+	notifyItem([ response ], ownerID, ownerNickName, publishDate, content,
+			postID, likeNum, postOwnerAvatar, postImage);
+}
+
+function notifyFollow(followerID) {
+	var data = FetchUserByID(followerID);
+	var tipFrame = '<div class="popTip notifyItem"><div class="content"><div class="urserBgShort"><img src="'
+			+ data.attributes.profileImageLink
+			+ '" id="remind-bell-profileImg"/></div><div class="urserInfShort"><img src="'
+			+ data.attributes.avatarLink
+			+ '" id="remind-bell-avatarImg"/><p><h1><a class="tipUser">'
+			+ data.attributes.name
+			+ '</a></h1></p><p>'
+			+ data.attributes.lookingFor + '</p></div></div></div>';
+	$(".mentionBody-content").empty();
+	$(".mentionBody-content").append(tipFrame);
+}
+
+function notifyItem(response, ownerID, ownerNickName, publishDate, content,
+		postID, likeNum, postOwnerAvatar, postImage) {
+	var comment = "";
+	$
+			.each(
+					response,
+					function(index, jsonComment) {
+						comment = comment
+								+ "<div class='act_content' id='"
+								+ jsonComment.ID
+								+ "'><div class='row'><div class='col-lg-1'><img src='"
+								+ jsonComment.owner.attributes.avatarLink
+								+ "' onload='javascript:auto_resize(30, 30, this)'/></div><div class='col-lg-10'><div class='col-lg-6 custom_lg-6'><div class='user_name'><strong>"
+								+ jsonComment.owner.attributes.name
+								+ "</strong></div></div><div class='col-lg-6 custom_lg-6'><div class='deleteCommBtn' style='cursor:pointer'><a><input id='"
+								+ postID
+								+ "' type='hidden' value='"
+								+ jsonComment.ID
+								+ "' /><span class='glyphicon glyphicon-remove' style='font-size: 8px'></span></a></div></div><div class='col-lg-5 custom_lg-6'><div class='user_info'>"
+								+ jsonComment.publishDate
+								+ "</div></div><div class='col-lg-5 custom_lg-6'><div class='comment_like' style='cursor: pointer'><div class='likeComment likeCommentN"
+								+ jsonComment.ID
+								+ "'>+<span>"
+								+ jsonComment.likerIDs.length
+								+ "</span></div><a><input id='likeID' type='hidden' value='"
+								+ jsonComment.ID
+								+ "' />+1<span style='font-size: 8px'></span></a></div></div><div class='col-lg-2'><div class='comment_reply' id="
+								+ postID
+								+ " style='cursor: pointer'><a><input id='replyName' type='hidden' value='"
+								+ jsonComment.owner.attributes.name
+								+ "' /><input id='replyID' type='hidden' value='"
+								+ jsonComment.ID
+								+ "' />reply<span style='font-size: 8px'></span></a></div></div></div></div><div class='act_comment'><a class='commentHead'>@"
+								+ jsonComment.attributes.commentToComment
+								+ "</a>" + "&nbsp;"
+								+ jsonComment.attributes.content
+								+ "﻿</div></div>";
+						if (USERID != jsonComment.owner.ID) {
+							$('.deleteCommBtn').css("display", "none");
+						}
+					});
+	var boarddiv = '<div class="post"'
+			+ postID
+			+ '" notifyItem"><div class="post_body"><div class="row"><div class="col-md-2"><div class="user_img">'
+			+ '<img class="userImg" src="'
+			+ postOwnerAvatar
+			+ '"/>'
+			+ '<input type="hidden" value="'
+			+ ownerID
+			+ '" name="userID"/></div></div><div class="col-md-6"><div class="user_name"><strong>'
+			+ ownerNickName
+			+ '</strong></div><div class="user_info">'
+			+ publishDate
+			+ '</div></div><div class="col-md-4"><div class="deletePostBtn"><a><input id="deleteID" type="hidden" value="'
+			+ postID
+			+ '" /><span class="glyphicon glyphicon-remove"></span></a></div></div></div><div class="post_info">'
+			+ content
+			+ '<div class="post_more"><a>read more...</a></div></div>';
+	if (postImage.length != 0)
+		boarddiv += '<div class="post_img"><img src="' + postImage[0]
+				+ '" /></div>';
+	boarddiv += '<div class="row"><div class="col-md-1"><div class="post_like" style="cursor:pointer"><a><input id="likeID" type="hidden" value="'
+			+ postID
+			+ '"><span class="glyphicon glyphicon-heart-empty" style="font-size:20px">'
+			+ likeNum
+			+ '</span></a></div></div><div class="col-md-1"><div class="post_collect" style="cursor:pointer"><a><input id="collectID" type="hidden" value="'
+			+ postID
+			+ '"><span class="glyphicon glyphicon-star-empty" style="font-size:20px"></span></a></div></div><div class="col-md-1"><div class="post_share" style="cursor:pointer"><a><span class="glyphicon glyphicon-share-alt" style="font-size:20px"></span></a></div></div></div><div class="media_comm"><div class="row addCommentBtn"><div class="col-lg-8"><div class="form-group"><input type="text" placeholder="Add a comment" class="form-control  commentTxt" id="commentText"'
+			+ postID
+			+ '" autocomplete="off"></div></div><div class="col-lg-4"><button type="submit" class="btn btn-success" id="addComment" value="'
+			+ postID
+			+ '">Submit</button></div></div>'
+			+ comment
+			+ '"</div></div></div>';
+	if (USERID != ownerID) {
+		$('.deletePostBtn').css("display", "none");
+	}
+	$(".mentionBody-content").empty();
+	$(".mentionBody-content").append(boarddiv);
+	$("#commentText" + postID).blur(function() {
+		$(this).attr("placeholder", "add a comment");
+	});
+}
