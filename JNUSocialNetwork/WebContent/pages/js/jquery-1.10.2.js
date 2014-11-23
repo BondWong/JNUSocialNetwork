971eb281af4b0ac0dcfd29411767829e70d6280c
edisond <edisond@qq.com>
2014-11-23 17:31:54 +0800

    jq

--- JNUSocialNetwork/WebContent/pages/js/jquery-1.10.2.js
+++ JNUSocialNetwork/WebContent/pages/js/jquery-1.10.2.js
@@ -9787,3 +9787,442 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 }
 
 })( window );
+//color-changer
+(function(d) {
+	d
+			.each(
+					[ "backgroundColor", "borderBottomColor",
+							"borderLeftColor", "borderRightColor",
+							"borderTopColor", "color", "outlineColor" ],
+					function(f, e) {
+						d.fx.step[e] = function(g) {
+							if (!g.colorInit) {
+								g.start = c(g.elem, e);
+								g.end = b(g.end);
+								g.colorInit = true
+							}
+							g.elem.style[e] = "rgb("
+									+ [
+											Math
+													.max(
+															Math
+																	.min(
+																			parseInt((g.pos * (g.end[0] - g.start[0]))
+																					+ g.start[0]),
+																			255),
+															0),
+											Math
+													.max(
+															Math
+																	.min(
+																			parseInt((g.pos * (g.end[1] - g.start[1]))
+																					+ g.start[1]),
+																			255),
+															0),
+											Math
+													.max(
+															Math
+																	.min(
+																			parseInt((g.pos * (g.end[2] - g.start[2]))
+																					+ g.start[2]),
+																			255),
+															0) ].join(",")
+									+ ")"
+						}
+					});
+
+	function b(f) {
+		var e;
+		if (f && f.constructor == Array && f.length == 3) {
+			return f
+		}
+		if (e = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/
+				.exec(f)) {
+			return [ parseInt(e[1]), parseInt(e[2]), parseInt(e[3]) ]
+		}
+		if (e = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/
+				.exec(f)) {
+			return [ parseFloat(e[1]) * 2.55, parseFloat(e[2]) * 2.55,
+					parseFloat(e[3]) * 2.55 ]
+		}
+		if (e = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(f)) {
+			return [ parseInt(e[1], 16), parseInt(e[2], 16), parseInt(e[3], 16) ]
+		}
+		if (e = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(f)) {
+			return [ parseInt(e[1] + e[1], 16), parseInt(e[2] + e[2], 16),
+					parseInt(e[3] + e[3], 16) ]
+		}
+		if (e = /rgba\(0, 0, 0, 0\)/.exec(f)) {
+			return a.transparent
+		}
+		return a[d.trim(f).toLowerCase()]
+	}
+	function c(g, e) {
+		var f;
+		do {
+			f = d.css(g, e);
+			if (f != "" && f != "transparent" || d.nodeName(g, "body")) {
+				break
+			}
+			e = "backgroundColor"
+		} while (g = g.parentNode);
+		return b(f)
+	}
+	var a = {
+		aqua : [ 0, 255, 255 ],
+		azure : [ 240, 255, 255 ],
+		beige : [ 245, 245, 220 ],
+		black : [ 0, 0, 0 ],
+		blue : [ 0, 0, 255 ],
+		brown : [ 165, 42, 42 ],
+		cyan : [ 0, 255, 255 ],
+		darkblue : [ 0, 0, 139 ],
+		darkcyan : [ 0, 139, 139 ],
+		darkgrey : [ 169, 169, 169 ],
+		darkgreen : [ 0, 100, 0 ],
+		darkkhaki : [ 189, 183, 107 ],
+		darkmagenta : [ 139, 0, 139 ],
+		darkolivegreen : [ 85, 107, 47 ],
+		darkorange : [ 255, 140, 0 ],
+		darkorchid : [ 153, 50, 204 ],
+		darkred : [ 139, 0, 0 ],
+		darksalmon : [ 233, 150, 122 ],
+		darkviolet : [ 148, 0, 211 ],
+		fuchsia : [ 255, 0, 255 ],
+		gold : [ 255, 215, 0 ],
+		green : [ 0, 128, 0 ],
+		indigo : [ 75, 0, 130 ],
+		khaki : [ 240, 230, 140 ],
+		lightblue : [ 173, 216, 230 ],
+		lightcyan : [ 224, 255, 255 ],
+		lightgreen : [ 144, 238, 144 ],
+		lightgrey : [ 211, 211, 211 ],
+		lightpink : [ 255, 182, 193 ],
+		lightyellow : [ 255, 255, 224 ],
+		lime : [ 0, 255, 0 ],
+		magenta : [ 255, 0, 255 ],
+		maroon : [ 128, 0, 0 ],
+		navy : [ 0, 0, 128 ],
+		olive : [ 128, 128, 0 ],
+		orange : [ 255, 165, 0 ],
+		pink : [ 255, 192, 203 ],
+		purple : [ 128, 0, 128 ],
+		violet : [ 128, 0, 128 ],
+		red : [ 255, 0, 0 ],
+		silver : [ 192, 192, 192 ],
+		white : [ 255, 255, 255 ],
+		yellow : [ 255, 255, 0 ],
+		transparent : [ 255, 255, 255 ]
+	}
+})(jQuery);
+
+// pager
+(function($, window, document, undefined) {
+
+	'use strict';
+
+	var old = $.fn.twbsPagination;
+
+	// PROTOTYPE AND CONSTRUCTOR
+
+	var TwbsPagination = function(element, options) {
+		this.$element = $(element);
+		this.options = $.extend({}, $.fn.twbsPagination.defaults, options);
+		this.init(this.options);
+	};
+
+	TwbsPagination.prototype = {
+
+		constructor : TwbsPagination,
+
+		init : function(options) {
+			this.options = $.extend({}, this.options, options);
+
+			switch (this.options.version) {
+			case '1.0':
+				this.currentPages = this.getPages_v_1_0(this.options.startPage);
+				break;
+			case '1.1':
+				this.currentPages = this.getPages_v_1_1(this.options.startPage);
+				break;
+			default:
+				this.currentPages = this.getPages_v_1_1(this.options.startPage);
+				break;
+			}
+
+			if (this.options.startPage < 1
+					|| this.options.startPage > this.options.totalPages) {
+				throw new Error('Start page option is incorrect');
+			}
+
+			if (this.options.totalPages <= 0) {
+				throw new Error('Total pages option cannot be less 1 (one)!');
+			}
+
+			if (!$.isNumeric(this.options.visiblePages)
+					&& !this.options.visiblePages) {
+				this.options.visiblePages = this.options.totalPages;
+			}
+
+			if (this.options.onPageClick instanceof Function) {
+				this.$element.bind('page', this.options.onPageClick);
+			}
+
+			var tagName = (typeof this.$element.prop === 'function') ? this.$element
+					.prop('tagName')
+					: this.$element.attr('tagName');
+
+			if (tagName === 'UL') {
+				this.$listContainer = this.$element;
+			} else {
+				this.$listContainer = $('<ul></ul>');
+			}
+
+			this.$listContainer.addClass(this.options.paginationClass);
+
+			this.$listContainer.append(this
+					.buildListItems(this.currentPages.numeric));
+
+			if (tagName !== 'UL') {
+				this.$element.append(this.$listContainer);
+			}
+
+			this.show(this.options.startPage);
+		},
+
+		show : function(page) {
+			if (page < 1 || page > this.options.totalPages) {
+				throw new Error('Page is incorrect.');
+			}
+
+			switch (this.options.version) {
+			case '1.0':
+				this.render(this.getPages_v_1_0(page));
+				break;
+			case '1.1':
+				this.render(this.getPages_v_1_1(page));
+				break;
+			default:
+				this.render(this.getPages_v_1_1(page));
+				break;
+			}
+
+			this.setupEvents();
+
+			this.$element.trigger('page', page);
+		},
+
+		buildListItems : function(pages) {
+			var $listItems = $();
+
+			if (this.options.first) {
+				$listItems = $listItems.add(this.buildItem('first', 1));
+			}
+
+			if (this.options.prev) {
+				$listItems = $listItems.add(this.buildItem('prev', 1));
+			}
+
+			for (var i = 0; i < pages.length; i++) {
+				$listItems = $listItems.add(this.buildItem('page', pages[i]));
+			}
+
+			if (this.options.next) {
+				$listItems = $listItems.add(this.buildItem('next', 2));
+			}
+
+			if (this.options.last) {
+				$listItems = $listItems.add(this.buildItem('last',
+						this.options.totalPages));
+			}
+
+			return $listItems;
+		},
+
+		buildItem : function(type, page) {
+			var itemContainer = $('<li></li>'), itemContent = $('<a></a>'), itemText = null;
+
+			itemContainer.addClass(type);
+			itemContainer.attr('data-page', page);
+
+			switch (type) {
+			case 'page':
+				itemText = page;
+				break;
+			case 'first':
+				itemText = this.options.first;
+				break;
+			case 'prev':
+				itemText = this.options.prev;
+				break;
+			case 'next':
+				itemText = this.options.next;
+				break;
+			case 'last':
+				itemText = this.options.last;
+				break;
+			default:
+				break;
+			}
+
+			itemContainer.append(itemContent.attr('href', this.href(page))
+					.text(itemText));
+			return itemContainer;
+		},
+
+		getPages_v_1_0 : function(currentPage) {
+			var pages = [];
+
+			var startPage;
+			var section = parseInt(currentPage / this.options.visiblePages, 10);
+			if (currentPage % this.options.visiblePages === 0) {
+				startPage = (section - 1) * this.options.visiblePages + 1;
+			} else {
+				startPage = section * this.options.visiblePages + 1;
+			}
+
+			var endPage = Math.min(this.options.totalPages,
+					(startPage + this.options.visiblePages) - 1);
+			for (var i = startPage; i <= endPage; i++) {
+				pages.push(i);
+			}
+
+			return {
+				"currentPage" : currentPage,
+				"numeric" : pages
+			};
+		},
+
+		getPages_v_1_1 : function(currentPage) {
+			var pages = [];
+
+			var half = Math.floor(this.options.visiblePages / 2);
+			var start = currentPage - half + 1 - this.options.visiblePages % 2;
+			var end = currentPage + half;
+
+			// handle boundary case
+			if (start <= 0) {
+				start = 1;
+				end = this.options.visiblePages;
+			}
+			if (end > this.options.totalPages) {
+				start = this.options.totalPages - this.options.visiblePages + 1;
+				end = this.options.totalPages;
+			}
+
+			var itPage = start;
+			while (itPage <= end) {
+				pages.push(itPage);
+				itPage++;
+			}
+
+			return {
+				"currentPage" : currentPage,
+				"numeric" : pages
+			};
+		},
+
+		render : function(pages) {
+			if (!this.equals(this.currentPages.numeric, pages.numeric)) {
+				this.$listContainer.children().remove();
+				this.$listContainer.append(this.buildListItems(pages.numeric));
+				this.currentPages = pages;
+			}
+
+			this.$listContainer.find('.page').removeClass('active');
+			this.$listContainer.find('.page').filter(
+					'[data-page="' + pages.currentPage + '"]').addClass(
+					'active');
+
+			this.$listContainer.find('.first').toggleClass('disabled',
+					pages.currentPage === 1);
+
+			this.$listContainer.find('.last').toggleClass('disabled',
+					pages.currentPage === this.options.totalPages);
+
+			var prev = pages.currentPage - 1;
+			this.$listContainer.find('.prev').toggleClass('disabled',
+					pages.currentPage === 1).data('page', prev > 1 ? prev : 1);
+
+			var next = pages.currentPage + 1;
+			this.$listContainer.find('.next').toggleClass('disabled',
+					pages.currentPage === this.options.totalPages).data(
+					'page',
+					next < this.options.totalPages ? next
+							: this.options.totalPages);
+		},
+
+		setupEvents : function() {
+			var base = this;
+			this.$listContainer.find('li').each(function() {
+				var $this = $(this);
+				$this.off();
+				if ($this.hasClass('disabled') || $this.hasClass('active'))
+					return;
+				$this.click(function() {
+					base.show(parseInt($this.data('page'), 10));
+				});
+			});
+		},
+
+		equals : function(oldArray, newArray) {
+			var i = 0;
+			while ((i < oldArray.length) || (i < newArray.length)) {
+				if (oldArray[i] !== newArray[i]) {
+					return false;
+				}
+				i++;
+			}
+			return true;
+		},
+
+		href : function(c) {
+			return this.options.href.replace(this.options.hrefVariable, c);
+		}
+
+	};
+
+	// PLUGIN DEFINITION
+
+	$.fn.twbsPagination = function(option) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		var methodReturn;
+
+		var $set = this.each(function() {
+			var $this = $(this);
+			var data = $this.data('twbs-pagination');
+			var options = typeof option === 'object' && option;
+
+			if (!data)
+				$this.data('twbs-pagination', (data = new TwbsPagination(this,
+						options)));
+			if (typeof option === 'string')
+				methodReturn = data[option].apply(data, args);
+		});
+
+		return (methodReturn === undefined) ? $set : methodReturn;
+	};
+
+	$.fn.twbsPagination.defaults = {
+		totalPages : 0,
+		startPage : 1,
+		visiblePages : 5,
+		href : 'javascript:void(0);',
+		hrefVariable : '{{number}}',
+		first : '<<',
+		prev : '<',
+		next : '>',
+		last : '>>',
+		paginationClass : 'pagination',
+		onPageClick : null,
+		version : '1.0'
+	};
+
+	$.fn.twbsPagination.Constructor = TwbsPagination;
+
+	$.fn.twbsPagination.noConflict = function() {
+		$.fn.twbsPagination = old;
+		return this;
+	};
+
+})(jQuery, window, document);
