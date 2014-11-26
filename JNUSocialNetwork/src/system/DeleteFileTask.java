0a8718c440dd273ccf180d685d0bee0a9f866826
JavaEEBoy <WongZeonbong@gmail.com>
2014-11-26 14:19:15 +0800

    wasted resource clear added

--- JNUSocialNetwork/src/system/DeleteFileTask.java
+++ JNUSocialNetwork/src/system/DeleteFileTask.java
@@ -7,11 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import model.structure.Image;
-
 import org.apache.commons.io.FileUtils;
-
-import utils.JsonUtil;
 
 public class DeleteFileTask implements Runnable {
 
@@ -26,22 +22,47 @@ public class DeleteFileTask implements Runnable {
 			e1.printStackTrace();
 		}
 		for (String link : DesertFileLinkMap.getLinks()) {
-			if (link == null || link == "")
+			System.out.println(link);
+			if (link == null || link.equals(""))
 				continue;
-			if (link.contains("src") && link.contains("width")
-					&& link.contains("height"))
-				link = JsonUtil.fromJson(link, Image.class).getSrc();
-
-			try {
-				if (Files.isDirectory(Paths.get(link)))
-					FileUtils.deleteDirectory(new File(link));
-				else
-					Files.deleteIfExists(Paths.get(link));
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			/*
+			 * else if (link.contains("originalImage") &&
+			 * link.contains("thumbnail") && link.contains("croppedImage")) {
+			 * ImageTuple tuple = JsonUtil.fromJson(link, ImageTuple.class);
+			 * String originalImage = tuple.getOriginalImage(); String
+			 * croppedImage = tuple.getCroppedImage(); String thumbnail =
+			 * tuple.getThumbnail(); if (!originalImage.equals(""))
+			 * originalImage = JsonUtil.fromJson(originalImage,
+			 * Image.class).getSrc(); if (!croppedImage.equals("")) croppedImage
+			 * = JsonUtil.fromJson(croppedImage, Image.class) .getSrc(); if
+			 * (!thumbnail.equals("")) thumbnail = JsonUtil.fromJson(thumbnail,
+			 * Image.class) .getSrc();
+			 * 
+			 * try { Files.deleteIfExists(Paths.get(originalImage));
+			 * Files.deleteIfExists(Paths.get(croppedImage));
+			 * Files.deleteIfExists(Paths.get(thumbnail)); } catch (IOException
+			 * e) { // TODO Auto-generated catch block e.printStackTrace(); }
+			 * continue; } else if (link.contains("src") &&
+			 * link.contains("width") && link.contains("height")) { link =
+			 * JsonUtil.fromJson(link, Image.class).getSrc(); try {
+			 * 
+			 * Files.deleteIfExists(Paths.get(link));
+			 * 
+			 * } catch (IOException e) { // TODO Auto-generated catch block
+			 * e.printStackTrace(); } continue; }
+			 */else {
+				try {
+					if (Files.isDirectory(Paths.get(link)))
+						FileUtils.deleteDirectory(new File(link));
+					else
+						Files.deleteIfExists(Paths.get(link));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				continue;
 			}
+
 		}
 		DesertFileLinkMap.removeLinks();
 		try {
